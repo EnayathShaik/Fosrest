6a1390e41695dbc50ae2293be3400a161c14b730
zentech-fostac <shaik.etu@gmail.com>
2017-06-19 09:54:25 +0530

    Query changes--org name(emaplled trainer)-supriya

--- Fosrest/src/com/ir/dao/impl/PageLoadDaoImpl.java
+++ Fosrest/src/com/ir/dao/impl/PageLoadDaoImpl.java
@@ -359,7 +359,7 @@ public class PageLoadDaoImpl implements PageLoadDao {
 		@Override
 		public List<TrainerForm> listTrainer() {
 			Session session = this.sessionFactory.getCurrentSession();
-			List<TrainerForm> mccList = session.createSQLQuery("select pit.firstName,pit.Email,pit.mobile,pit.correspondenceAddress1,c.cityname,d.districtname,s.statename,pit.orgName from personalinformationtrainer pit inner join StateMaster s on cast(pit.correspondencestate as numeric ) =s.stateid inner join DistrictMaster d on cast(pit.correspondencedistrict as numeric ) =d.districtid inner join CityMaster c on cast(pit.correspondencecity as numeric ) =c.cityid where pit.createdBy=4").list();
+			List<TrainerForm> mccList = session.createSQLQuery("select pit.firstName,pit.Email,pit.mobile,pit.correspondenceAddress1,c.cityname,d.districtname,s.statename from personalinformationtrainer pit inner join StateMaster s on cast(pit.correspondencestate as numeric ) =s.stateid inner join DistrictMaster d on cast(pit.correspondencedistrict as numeric ) =d.districtid inner join CityMaster c on cast(pit.correspondencecity as numeric ) =c.cityid where pit.createdBy=4").list();
 			return mccList;
 		}
 	
